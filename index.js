b5ea12279aaec69cfcf6198ada82ac9d98d375fd
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2025-02-04 02:58:43 -0300

    ✨ feat : http module

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { Person } = require("./person");
-const person = new Person("Dionatan");
 
+const person = new Person("Dionatan");
 // console.log(person.sayMyName());
+
 // require ("./modules/path")
- require("./modules/fs");
+// require("./modules/fs");
+require("./modules/http");
+
